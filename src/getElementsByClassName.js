b910058fde2e4a29e044f46de04b5dd2e030ae74
Zachpierce15 <zachary.15pierce@gmail.com>
2019-04-08 22:18:19 -0700

    This is all of our work so far

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // create var that points to current node
+  var theBod = document.body
+  var results = []
+  function checkClass (node) {
+   node = node || theBod
+    var classes = node.classList;
+    if (classes.contains(className)) {
+      results.push(node)
+    } 
+    if(node.childNodes) {
+      for(var i = 0; i < node.children.length; i++) {
+        checkClass(node.children[i])
+      }
+    }
+  }
+  
+console.log(results)
+  return results
 };
+
+// var htmlStrings = [
+//   '<div class="targetClassName"></div>',
+//   '<div class="otherClassName targetClassName"></div>',
+//   '<div><div class="targetClassName"></div></div>',
+//   '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+//   '<div><div></div><div><div class="targetClassName"></div></div></div>',
+//   '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+//   '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+// ];
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,58 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+var result;
+  // check type of elements
+  // use typeof to check  
+  if ( typeof obj === "number" || typeof obj === "boolean" || typeof obj === "undefined"){
+     result = '' + obj
+  }
+  if (typeof obj === "function" || obj === undefined) {
+  return '{}'
+  }
+
+  if (obj === null || obj === NaN){
+    result = null + ''
+     return result
+    }
+  if ( typeof obj === "string") {
+   result = '"' + obj + '"' 
+}
+  if (Array.isArray(obj)){
+    if (obj.length < 1) {
+     return '[]'
+  }
+   var arrElem =[];
+   for(var i = 0; i < obj.length; i++){
+     arrElem.push(stringifyJSON(obj[i]))
+  }
+  return result = '[' + arrElem + ']'
+ } 
+
+  else if (typeof obj === "object" && obj !== undefined) {
+    var objStor = []
+    if (Object.keys(obj).length === 0) {
+      return '{}'
+      }
+    if (obj !== 'undefined' || obj !== 'functions') {
+    for (var keys in obj) {
+      if (typeof obj[keys] === "function" || typeof obj[keys] === "undefined"){
+        continue
+      }
+      objStor.push(stringifyJSON(keys) + ":" + stringifyJSON(obj[keys])) 
+    }
+  }
+  result = '{' + objStor + '}'
+}
+
+  return result
+
 };
+
+
+unstringifiableValues = [
+  {
+    'functions': function() {},
+    'undefined': undefined
+  }
+];
